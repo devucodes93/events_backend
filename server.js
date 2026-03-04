1e3b2aca6c95619316b931adb748d1ebffcfc6c0
mounesh <mouneshv696@gmail.com>
2026-03-04 14:20:16 +0530

    small bug

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ import express from "express";
 import connectDb from "./config/db.js";
 import dotenv from "dotenv";
 import user from "./router/User.js";
-import product from "./router/Product.js";
-import cart from "./router/Cart.js";
 dotenv.config();
 
 const app = express();
@@ -12,13 +10,7 @@ const port =3000;
 connectDb();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ msg: "hi" });
-});
-
 app.use("/api/auth",user)
-app.use("/api/product",product)
-app.use("/api/cart",cart)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
